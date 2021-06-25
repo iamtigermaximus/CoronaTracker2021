d50c4e9a93fae45817815dccbd63ee8c3930ac8f
Siegfred <dante_sparda1925@yahoo.com>
2021-06-26 02:24:55 +0300

    first commit

--- src/components/CountryAdvisoryCard.js
+++ src/components/CountryAdvisoryCard.js
@@ -6,7 +6,7 @@ const CountryAdvisoryCard = (props) => {
             <div className="card bg-light text-dark ">
                 <div className="card_header pt-3"><h2>{props.country}</h2></div>
                 <div className="card-body">
-                     <h5 className="card-title">Risk Level Score: {props.riskLevel}</h5>
+                <h5 className="card-title">Risk Level Score: {props.riskLevel}</h5>
                     <p className="card-text">Advisory: {props.advisory}</p>
                 </div>
                 <div className="card-footer bg-transparent border-success">Last Updated {props.updated}</div>
--- src/components/TravelAdvisory.js
+++ src/components/TravelAdvisory.js
@@ -4,8 +4,12 @@ import CountryAdvisoryCard from '../components/CountryAdvisoryCard'
 
 const TravelAdvisory = () => {
 const [advisories, setAdvisories]= useState("")
+const [searchCountry,setSearchCountry] = useState("")
 
-
+const handleChange = (e)=>{
+    e.preventDefault()
+    setSearchCountry(e.target.value)
+}
 useEffect(()=>{
     axios.get("https://www.travel-advisory.info/api?countrycode")
     .then(response => {
@@ -19,16 +23,32 @@ useEffect(()=>{
 },[])
     const advisoriesArray = Object.values(advisories);
     console.log(advisoriesArray)
+
+    const filteredAdvisories= advisoriesArray.filter(c =>{
+        return c.name.toLowerCase().includes(searchCountry.toLowerCase())
+    })
     return (
        <div className="travel-advisories-container">
            <div className="travel-advisory-header container text-center">
            <div className="title my-4">
                <h1>Travel Advisory</h1>
            </div>
+           <div className="container">
+            <div className="form">
+                <div className="p-5">
+                    <input 
+                    className="form-control" 
+                    type="text" 
+                    placeholder="Search a Country"
+                    onChange={handleChange}
+                    />
+                </div>
+            </div>
+        </div>
            <div className="country-advisory-container pb-5">
                <div className="container">
                    <div className="row">
-                       {advisoriesArray.map((advisory,value)=>(
+                       {filteredAdvisories.map((advisory)=>(
                            <div className="col-md-6">
                                <CountryAdvisoryCard
                                key={advisory.iso_alpha2}
